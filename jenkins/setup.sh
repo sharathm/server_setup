840e3f90537fedb12b29709586c1302b37494f05
Sharath <mailsharath@gmail.com>
2021-08-13 12:54:24 +0530

    Create setup.sh

---
+++ jenkins/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# create the docker network
+docker network create jenkins
+
+# install docker dind
+docker run \
+  --name jenkins-docker \
+  --rm \
+  --detach \
+  --privileged \
+  --network jenkins \
+  --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  docker:dind \
+  --storage-driver overlay2
+  
+  # build the jenkins image
+  docker build -t myjenkins-blueocean:1.1 .
